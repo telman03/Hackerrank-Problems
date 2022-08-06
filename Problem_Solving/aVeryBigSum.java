22cee35c81a60e707c6a414adf88dac2059cfa0b
telman03 <telmangadimov1@gmail.com>
2022-08-06 13:55:49 +0400

    update

--- Problem_Solving/aVeryBigSum.java
+++ Problem_Solving/aVeryBigSum.java
@@ -1,5 +1,17 @@
 package Problem_Solving;
 
-public class aVeryBigSum {
+import java.util.List;
 
+/**
+ * https://www.hackerrank.com/challenges/a-very-big-sum/problem?isFullScreen=false
+ */
+
+public class aVeryBigSum {
+    public static long aVeryBigSum(List<Long> ar) {
+        long a = 0;
+        for(int i = 0; i < ar.size(); i++){
+            a += ar.get(i);
+        }
+        return a;
+    }
 }
