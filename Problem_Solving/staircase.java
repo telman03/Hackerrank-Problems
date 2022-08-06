8f3a4a7320df61da67e067434ff0d4d94a724d57
telman03 <telmangadimov1@gmail.com>
2022-08-06 19:09:32 +0400

    update

---
+++ Problem_Solving/staircase.java
@@ -0,0 +1,17 @@
+package Problem_Solving;
+
+public class staircase {
+    public static void main(String[] args) {
+        int n = 6;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(j<n-1-i){
+                    System.out.print(" ");
+                }else{
+                    System.out.print("#");
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
