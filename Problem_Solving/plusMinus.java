d2103b5c6b0f889a21b345c8c6fcd6f0f84479dd
telman03 <telmangadimov1@gmail.com>
2022-08-06 15:58:58 +0400

    update

---
+++ Problem_Solving/plusMinus.java
@@ -0,0 +1,29 @@
+package Problem_Solving;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class plusMinus {
+    public static void plusMinus(List<Integer> arr) {
+        double positive = 0, negative = 0, zero = 0;
+        int arraySize = arr.size();
+
+
+        for (int i = 0; i < arr.size(); i++) {
+
+            if (arr.get(i) > 0) {
+                positive++;
+            } else if (arr.get(i) < 0) {
+                negative++;
+            } else {
+                zero++;
+            }
+        }
+
+
+        System.out.println(positive / arraySize);
+        System.out.println(negative / arraySize);
+        System.out.println(zero / arraySize);
+
+    }
+}
