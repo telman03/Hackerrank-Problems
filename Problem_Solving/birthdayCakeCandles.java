555db9ec4d6e8b3103c855bd7beb4e4e7f6bfd56
telman03 <telmangadimov1@gmail.com>
2022-08-07 10:06:56 +0400

    update

---
+++ Problem_Solving/birthdayCakeCandles.java
@@ -0,0 +1,24 @@
+package Problem_Solving;
+
+import java.util.List;
+
+public class birthdayCakeCandles {
+    public static int birthdayCakeCandles(List<Integer> candles) {
+        int x = 0;
+        int y = 0;
+
+        for(int candle : candles){
+            if(candle > x){
+                x = candle;
+            }
+        }
+
+        for(int candle : candles){
+            if(candle == x){
+                y++;
+            }
+        }
+
+        return y;
+    }
+}
