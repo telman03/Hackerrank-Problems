7fdc84255ad49b8a48d3754077908e85ae1dc088
telman03 <telmangadimov1@gmail.com>
2022-08-09 18:36:12 +0400

    update

--- Implementation/gradingStudents.java
+++ Implementation/gradingStudents.java
@@ -1,12 +1,19 @@
 package Implementation;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class gradingStudents {
     public static List<Integer> gradingStudents(List<Integer> grades) {
-
-
-
-        return null;
+        List<Integer> ans = new ArrayList<Integer>();
+        for(int marks:grades){
+            if(marks < 38 || marks % 5 <= 2){
+                ans.add(marks);
+            }else if(marks % 5 > 2){
+                int add = 5 -(marks % 5);
+                ans.add(marks + add);
+            }
+        }
+        return ans;
     }
 }
