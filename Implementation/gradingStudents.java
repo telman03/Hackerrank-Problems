18cd72173f4dbfa07656e3f417bcf1a4361548ca
Telman Gadimov <70106877+telman03@users.noreply.github.com>
2022-08-24 17:36:37 +0400

    Update gradingStudents.java

--- Implementation/gradingStudents.java
+++ Implementation/gradingStudents.java
@@ -7,6 +7,7 @@ public class gradingStudents {
     public static List<Integer> gradingStudents(List<Integer> grades) {
         List<Integer> ans = new ArrayList<Integer>();
         for(int marks:grades){
+            
             if(marks < 38 || marks % 5 <= 2){
                 ans.add(marks);
             }else if(marks % 5 > 2){
